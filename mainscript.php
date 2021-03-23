c8dc63cfc24fbf5d554b0423ace630608672db24
alvinAdler <alvinadlersampow@gmail.com>
2021-03-23 18:15:15 +0700

    Break the JSON data from JS
    All data from one JSON string from JS has been separated into multiple variables. Username, line_coordinates, line_colors, and line_weights are separated into different variables with type string

--- mainscript.php
+++ mainscript.php
@@ -8,9 +8,25 @@
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $str_json = file_get_contents('php://input');
         $json_array = json_decode($str_json);
+        $record_line_coordinates = "";
+        $record_line_colors = "";
+        $record_line_weights = "";
 
         ////This is an example regarding how to take a data from json_array. 
         ////In this case, we are taking the line color of the first line.
         // echo $json_array->line_data[0]->line_color;
+
+        $current_username = $json_array->username;
+
+        foreach($json_array->line_data as $current_data){
+            $record_line_coordinates .= implode(",", $current_data->line_coordinates);
+            $record_line_coordinates .= "|";
+            $record_line_colors .= $current_data->line_color . "|";
+            $record_line_weights .= $current_data->line_weight . "|";
+        }
+        
+        echo $record_line_coordinates;
+        echo $record_line_colors;
+        echo $record_line_weights;
     }
 ?>
